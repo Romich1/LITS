e12263f1932ee07e65665f9ba1f7c8a59cfb9db5
Romich1 <roman@softsystems.com.ua>
2015-10-25 20:01:25 +0200

    random choice from list changed to 'choice()' func

--- cardsgame.py
+++ cardsgame.py
@@ -1,252 +1,75 @@
-import random
+import random as r
+deck = [6, 7, 8, 9, 10, 2, 3, 4,11]*4
+dealer = []
+player = []
+c = ''
+    
+def showHand():
+    hand = 0
+    for i in player: 
+        hand += i #Tally up the total
+    #print "The dealer has  %d" % dealer[0]
+    print "Your hand totals: %d (%s)" % (hand, player)
 
-	
-def makecardslist():
-	
-	lst = []	
-	for card in lstcards:
-		for mast in lstlears: 
-			lst.append(card+mast)
-	return lst
+#Gives player and dealer their cards
+def setup():
+    for i in range(2):
+        dealDealer = r.choice(deck)
+        dealPlayer = r.choice(deck)
+        dealer.append(dealDealer)
+        player.append(dealPlayer)
+        deck.pop(dealDealer)
+        deck.pop(dealPlayer)
 
-#makes full cards list and sort it by value	
-def lstforsorting(trumplear):
-	
-	lst = []	
-	lstlears.remove(trumplear)
-	random.shuffle(lstlears)
-	
-	for card in lstcards:
-		for lear in lstlears: 		
-			lst.append(card+lear)
-	
-	for card in lstcards:
-			lst.append(card+trumplear)		
-
-	return lst	
-	
-#takes card from list. 
-#next card if randomchoice = False 
-#random card if randomchoice = True 	
-def cardfromlst(lstfrom,lstto,amount,randomchoice=False):
-
-	amount = min(amount,len(lstfrom)) 
-	if amount < 1: 
-		return False
-	
-	if randomchoice: 
-		card = random.choice(lstfrom) 
-	else:
-		card = lstfrom[0]
-		
-	lstto.append(card)
-	lstfrom.remove(card)
-
-	if amount > 1:
-		cardfromlst(lstfrom,lstto,amount-1,randomchoice) 
-
-	return True
-
-#defines direction for first step (which card list moves firts)
-def definedirection(lst1,lst2):
-
-	trumpslst1 = []
-	trumpslst2 = [] 
-
-	for card1 in lst1:
-		if card1[1] != trumpcard[1]:
-			continue 
-		trumpslst1.append(card1)
-
-	for card2 in lst2:
-		if card2[1] != trumpcard[1]:
-			continue 
-		trumpslst2.append(card2) 
-
-	if len(trumpslst1)==0: return True
-	if len(trumpslst2)==0: return False
-	trumpslst1.sort(key=lambda x: sortinglst.index(x))
-	trumpslst2.sort(key=lambda x: sortinglst.index(x))
-
-	return sortinglst.index(trumpslst1[0]) <  sortinglst.index(trumpslst2[0])
-	
-#move
-#if automuve=True: comp make move, 
-#if automuve=False:  user makee choice
-def makestep(lstfrom,playlist,automove=True): 
-	
-	if len(lstfrom) == 0:	
-		return False 
-		
-	lstfrom.sort(key=lambda x: sortinglst.index(x))  
-	
-	if automove:
-		card = ''	
-	else: 
-		printgamestep(lstfrom,playlist,True)
-		card = input('input card or 0 for pass: ')
-		if card == '0' and len(playlist) > 0: return False
-
-	if len(playlist) == 0: # first step:
-		
-		if automove:
-			
-			#searching pairs firstly, prioritize firsts cards pair
-			if len(lstfrom) > 8 and lstfrom[3][0] == lstfrom[4][0]:
-				card = lstfrom[3] 	
-			if len(lstfrom) > 6 and lstfrom[2][0] == lstfrom[3][0]:
-				card = lstfrom[2] 	
-			if len(lstfrom) > 2 and lstfrom[1][0] == lstfrom[2][0]:
-				card = lstfrom[1] 	
-
-			if card=='': 
-				card = lstfrom[0]
-		else:
-			if not card in lstfrom: 
-				print('You have to choice correct card. Try again') 
-				return makestep(lstfrom,playlist,False)	
-	
-	else:
-		
-		lstadding = []
-		for playcard in playlist:
-			for lstcard in lstfrom:
-				if lstcard[0] == playcard[0]:
-					lstadding.append(lstcard)
-		
-		if automove:
-			lstadding.sort(key=lambda x: sortinglst.index(x))
-			if len(lstadding) != 0:
-				card = lstadding[0]	
-				if card[1] == trumpcard[1]:
-					if len(lst)/2 > 8-lstcards.index(card[0]): 
-						card = ''				
-		else:
-			if not card in lstadding: 
-				print('You have to choice correct card. Try again') 
-				return makestep(lstfrom,playlist,False)		
-		
-	if card == '':
-		return False
-	else:			
-		playlist.append(card)
-		lstfrom.remove(card)			
-		return True 	
-		
-#countermove	
-#if cardtochek is not empty - cheking can it be used or not	
-def answerstep(lstfrom,playlist,automove=True):
-
-	if len(lstfrom) == 0 or len(playlist) == 0:
-		return False 
-	
-	lstfrom.sort(key=lambda x: sortinglst.index(x)) 
-
-	hitlist = []
-	for card in lstfrom:
-		if card[1] == playlist[-1][1]: 
-			if lstcards.index(card[0]) > lstcards.index(playlist[-1][0]):
-				hitlist.append(card)
-		else:
-			if playlist[-1][1] != trumpcard[1] and card[1] == trumpcard[1]:
-				hitlist.append(card)
-	
-	if automove:	
-		hitlist.sort(key=lambda x: sortinglst.index(x))		
-		card = ''
-		if len(hitlist) > 0: 		
-			card = hitlist[0]
-			if card[1] == trumpcard[1]:
-				if len(lst)/2 > 9-lstcards.index(card[0]): 
-						card = ''
-
-	else:
-		printgamestep(lstfrom,playlist,False)
-		card = input('input card or 0 for taking playlist: ')
-		if card != '0' and not card in hitlist: 	
-			print('You have to choice correct card. Try again') 
-			return answerstep(lstfrom,playlist,False)
-		
-	if card == '' or card == '0':
-		lstfrom.extend(playlist)
-		return False 
-	else:
-		playlist.append(hitlist[0])
-		lstfrom.remove(hitlist[0])
-		return True
-
-def printstatus(mycards,compcards,lst,sometext=''):
-	
-	print('\n'+sometext)
-	print('my - '+str(len(mycards))+' '+str(mycards))
-	print('comp - '+str(len(compcards))+' '+str(compcards))
-	print('lst - '+str(len(lst))+' '+str(lst))
-		
-def printgamestep(mycards,playlist,directon):
-	
-	if directon:
-		sometext = 'Your move '
-	else:
-		sometext = 'Your answer '
-
-	print('\n'+'Batch '+str(step)+' '+sometext)
-	print('cards - '+str(len(mycards))+' '+str(mycards))
-	print('playlist - '+str(len(playlist))+' '+str(playlist))
-	print('cards left - '+str(len(lst))+' ; trumpcard - '+str(trumpcard))	
-			
-print('============================================')
-		
-lstlears = ['h','d','c','s']
-lstcards = ['6','7','8','9','0','j','q','k','a']
-
-lst = makecardslist()
-random.shuffle(lst) 
-trumpcard = lst[-1]
-sortinglst = lstforsorting(trumpcard[1])
-
-mycards = []
-compcards = []
-cardfromlst(lst,mycards,6-len(mycards))
-cardfromlst(lst,compcards,6-len(compcards))
-mycards.sort(key=lambda x: sortinglst.index(x))
-compcards.sort(key=lambda x: sortinglst.index(x))
-
-direction = definedirection(mycards,compcards)
-step = 0
-
-while len(compcards) != 0 and len(mycards) != 0:
-	
-	step += 1
-	playlist = [] #list of current game session (before pass out)
-	
-	if direction:
-		firstlst = mycards
-		secondlst = compcards 
-	else:	
-		firstlst = compcards
-		secondlst = mycards 
-	
-	game = True
-	while game: 		
-		if makestep(firstlst,playlist, not direction): 
-			if answerstep(secondlst,playlist, direction):
-				game = True
-			else:	
-				game = False
-		else:	
-			game = False	
-			direction = not direction
-
-	cardfromlst(lst,firstlst,6-len(firstlst))
-	cardfromlst(lst,secondlst,6-len(secondlst))	
-	print('Batch '+str(step)+' ended.')
-
-if len(mycards) == 0 and len(compcards) == 0:
-	status = 'CHOICE'	
-elif len(mycards) > 0: 
-	status = 'COMP WIN'	
-elif len(compcards) > 0: 
-	status = 'YOU WIN'		
-
-print('\n----------- '+status+' -------------')
-printstatus(mycards,compcards,lst,'left cards:')
+setup()
+while c != 'q':
+    showHand()
+    c = raw_input("[H]it [S]tand [Q]uit: ").lower()
+   # clear()
+    if c == 'h':
+        dealPlayer = r.choice(deck)
+        player.append(dealPlayer)
+        deck.pop(dealPlayer)
+        hand = 0
+        for i in dealer: hand += i
+        if not hand > 17:   #Dealer strategy.
+            dealDealer = r.choice(deck)
+            dealer.append(dealDealer)
+            deck.pop(dealDealer)
+        hand = 0
+        for i in player: 
+            hand += i
+            if hand > 21:
+                print "Dealer won"
+                player = []     #Clear player hand
+                dealer = []     #Clear dealer's hand
+                setup()         #Run the setup again
+        hand = 0
+        for i in dealer: 
+            hand +=i
+            if hand > 21:
+                print "Dealer lost!"
+                player = []
+                dealer = []
+                setup()
+    elif c == 's':
+        dHand = 0           #Dealer's hand total
+        pHand = 0           #Player's hand total
+        for i in dealer: dHand += i
+        for i in player: pHand += i
+        if pHand > dHand:
+            print "You won. Congradulations"    #If playerHand (pHand) is greater than dealerHand (dHand) you win...
+            dealer = []
+            player = []
+            setup()
+        else:
+            print "You lost. I`m sorry." 
+            print 'Dealers hand is ' + str(dHand)
+            dealer = []
+            player = []
+            setup()
+    else:
+        if c == 'q':
+            gb = raw_input("Bye. [Hit Enter]")
+        else:
+            print "Invalid choice."
