49cd02b837025cc04340e36921021ff88f71e3f5
Romich1 <roman@softsystems.com.ua>
2015-10-25 14:22:25 +0200

    Update cardsgame.py

--- cardsgame.py
+++ cardsgame.py
@@ -1,14 +1,15 @@
 import random
-		
 
+	
 def makecardslist():
 	
 	lst = []	
-	for mast in lstlears: 
-		for card in lstcards:
-			lst.append(mast+card)
+	for card in lstcards:
+		for mast in lstlears: 
+			lst.append(card+mast)
 	return lst
-	
+
+#makes full cards list and sort it by value	
 def lstforsorting(trumplear):
 	
 	lst = []	
@@ -17,21 +18,19 @@ def lstforsorting(trumplear):
 	
 	for card in lstcards:
 		for lear in lstlears: 		
-			lst.append(lear+card)
+			lst.append(card+lear)
 	
 	for card in lstcards:
-			lst.append(trumplear+card)		
+			lst.append(card+trumplear)		
 
 	return lst	
 	
-
 #takes card from list. 
 #next card if randomchoice = False 
 #random card if randomchoice = True 	
 def cardfromlst(lstfrom,lstto,amount,randomchoice=False):
 
 	amount = min(amount,len(lstfrom)) 
-
 	if amount < 1: 
 		return False
 	
@@ -48,50 +47,133 @@ def cardfromlst(lstfrom,lstto,amount,randomchoice=False):
 
 	return True
 
+#defines direction for first step (which card list moves firts)
+def definedirection(lst1,lst2):
+
+	trumpslst1 = []
+	trumpslst2 = [] 
+
+	for card1 in lst1:
+		if card1[1] != trumpcard[1]:
+			continue 
+		trumpslst1.append(card1)
+
+	for card2 in lst2:
+		if card2[1] != trumpcard[1]:
+			continue 
+		trumpslst2.append(card2) 
+
+	if len(trumpslst1)==0: return True
+	if len(trumpslst2)==0: return False
+	trumpslst1.sort(key=lambda x: sortinglst.index(x))
+	trumpslst2.sort(key=lambda x: sortinglst.index(x))
+
+	return sortinglst.index(trumpslst1[0]) <  sortinglst.index(trumpslst2[0])
+	
 #move
-def makestep(lstfrom,playlist): 
+#if automuve=True: comp make move, 
+#if automuve=False:  user makee choice
+def makestep(lstfrom,playlist,automove=True): 
 	
 	if len(lstfrom) == 0:	
 		return False 
 		
 	lstfrom.sort(key=lambda x: sortinglst.index(x))  
-		
-	if len(playlist) == 0: # first step:
-		card = lstfrom[0]
-	else:
-		card = random.choice(lstfrom);
-		
-	playlist.append(card)
-	lstfrom.remove(card)			
 	
-	return True 
+	if automove:
+		card = ''	
+	else: 
+		printgamestep(lstfrom,playlist,True)
+		card = input('input card or 0 for pass: ')
+		if card == '0' and len(playlist) > 0: return False
 
-#countermove		
-def answerstep(lstfrom,playlist):
+	if len(playlist) == 0: # first step:
+		
+		if automove:
+			
+			#searching pairs firstly, prioritize firsts cards pair
+			if len(lstfrom) > 8 and lstfrom[3][0] == lstfrom[4][0]:
+				card = lstfrom[3] 	
+			if len(lstfrom) > 6 and lstfrom[2][0] == lstfrom[3][0]:
+				card = lstfrom[2] 	
+			if len(lstfrom) > 2 and lstfrom[1][0] == lstfrom[2][0]:
+				card = lstfrom[1] 	
+
+			if card=='': 
+				card = lstfrom[0]
+		else:
+			if not card in lstfrom: 
+				print('You have to choice correct card. Try again') 
+				return makestep(lstfrom,playlist,False)	
+	
+	else:
+		
+		lstadding = []
+		for playcard in playlist:
+			for lstcard in lstfrom:
+				if lstcard[0] == playcard[0]:
+					lstadding.append(lstcard)
+		
+		if automove:
+			lstadding.sort(key=lambda x: sortinglst.index(x))
+			if len(lstadding) != 0:
+				card = lstadding[0]	
+				if card[1] == trumpcard[1]:
+					if len(lst)/2 > 8-lstcards.index(card[0]): 
+						card = ''				
+		else:
+			if not card in lstadding: 
+				print('You have to choice correct card. Try again') 
+				return makestep(lstfrom,playlist,False)		
+		
+	if card == '':
+		return False
+	else:			
+		playlist.append(card)
+		lstfrom.remove(card)			
+		return True 	
+		
+#countermove	
+#if cardtochek is not empty - cheking can it be used or not	
+def answerstep(lstfrom,playlist,automove=True):
 
 	if len(lstfrom) == 0 or len(playlist) == 0:
 		return False 
-		
+	
+	lstfrom.sort(key=lambda x: sortinglst.index(x)) 
+
 	hitlist = []
 	for card in lstfrom:
-		if card[0] == playlist[-1][0]: 
-			if lstcards.index(card[1]) > lstcards.index(playlist[-1][1]):
+		if card[1] == playlist[-1][1]: 
+			if lstcards.index(card[0]) > lstcards.index(playlist[-1][0]):
 				hitlist.append(card)
 		else:
-			if playlist[-1][0] != trumpcard[0] and card[0] == trumpcard[0]:
+			if playlist[-1][1] != trumpcard[1] and card[1] == trumpcard[1]:
 				hitlist.append(card)
+	
+	if automove:	
+		hitlist.sort(key=lambda x: sortinglst.index(x))		
+		card = ''
+		if len(hitlist) > 0: 		
+			card = hitlist[0]
+			if card[1] == trumpcard[1]:
+				if len(lst)/2 > 9-lstcards.index(card[0]): 
+						card = ''
+
+	else:
+		printgamestep(lstfrom,playlist,False)
+		card = input('input card or 0 for taking playlist: ')
+		if card != '0' and not card in hitlist: 	
+			print('You have to choice correct card. Try again') 
+			return answerstep(lstfrom,playlist,False)
 		
-	hitlist.sort(key=lambda x: sortinglst.index(x))
-	print(hitlist)
-	
-	if len(hitlist) == 0: 
+	if card == '' or card == '0':
 		lstfrom.extend(playlist)
-		return False
-	 
-	playlist.append(hitlist[0])
-	lstfrom.remove(hitlist[0])
-	
-	return True
+		return False 
+	else:
+		playlist.append(hitlist[0])
+		lstfrom.remove(hitlist[0])
+		return True
 
 def printstatus(mycards,compcards,lst,sometext=''):
 	
@@ -100,18 +182,27 @@ def printstatus(mycards,compcards,lst,sometext=''):
 	print('comp - '+str(len(compcards))+' '+str(compcards))
 	print('lst - '+str(len(lst))+' '+str(lst))
 		
+def printgamestep(mycards,playlist,directon):
+	
+	if directon:
+		sometext = 'Your move '
+	else:
+		sometext = 'Your answer '
+
+	print('\n'+'Batch '+str(step)+' '+sometext)
+	print('cards - '+str(len(mycards))+' '+str(mycards))
+	print('playlist - '+str(len(playlist))+' '+str(playlist))
+	print('cards left - '+str(len(lst))+' ; trumpcard - '+str(trumpcard))	
 			
 print('============================================')
 		
 lstlears = ['h','d','c','s']
 lstcards = ['6','7','8','9','0','j','q','k','a']
 
-#lstnew = makecardslist(lstlears,lstcards)
-#lst = shufflecards(lstnew) 
 lst = makecardslist()
 random.shuffle(lst) 
 trumpcard = lst[-1]
-sortinglst = lstforsorting(trumpcard[0])
+sortinglst = lstforsorting(trumpcard[1])
 
 mycards = []
 compcards = []
@@ -120,15 +211,13 @@ cardfromlst(lst,compcards,6-len(compcards))
 mycards.sort(key=lambda x: sortinglst.index(x))
 compcards.sort(key=lambda x: sortinglst.index(x))
 
-direction = True
+direction = definedirection(mycards,compcards)
 step = 0
 
 while len(compcards) != 0 and len(mycards) != 0:
 	
+	step += 1
 	playlist = [] #list of current game session (before pass out)
-	
-	addtext = 'step - '+str(step)+'  direction - ' + str(direction) 
-	printstatus(mycards,compcards,lst,addtext)
 	
 	if direction:
 		firstlst = mycards
@@ -137,13 +226,27 @@ while len(compcards) != 0 and len(mycards) != 0:
 		firstlst = compcards
 		secondlst = mycards 
 	
-	makestep(firstlst,playlist)	
-	if answerstep(secondlst,playlist):
-		direction = not direction
+	game = True
+	while game: 		
+		if makestep(firstlst,playlist, not direction): 
+			if answerstep(secondlst,playlist, direction):
+				game = True
+			else:	
+				game = False
+		else:	
+			game = False	
+			direction = not direction
 
 	cardfromlst(lst,firstlst,6-len(firstlst))
 	cardfromlst(lst,secondlst,6-len(secondlst))	
-	step += 1	
+	print('Batch '+str(step)+' ended.')
 
-print('\n------------------------')
-printstatus(mycards,compcards,lst)
+if len(mycards) == 0 and len(compcards) == 0:
+	status = 'CHOICE'	
+elif len(mycards) > 0: 
+	status = 'COMP WIN'	
+elif len(compcards) > 0: 
+	status = 'YOU WIN'		
+
+print('\n----------- '+status+' -------------')
+printstatus(mycards,compcards,lst,'left cards:')
